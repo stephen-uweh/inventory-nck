6dd28ed92563df75229751d0f2f91c8b5783d436
SteveCode <SteveCode@LEVELS>
2022-07-12 20:04:15 +0100

    Final push

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function(){
+    return "Inventory API for NCK Technologies";
+});
+
 
 // Auth Routes
 Route::group(["prefix" => "auth"], function () {
